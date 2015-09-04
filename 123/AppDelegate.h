665d80ed8b3be9eeff042c8dd8bd915af9019677
mygithubMYLOVE <lanou3g@lanou3gdeMac-mini-6.local>
2015-09-04 22:24:47 +0800

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by lanou3g on 15/9/4.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by lanou3g on 15/9/4.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
